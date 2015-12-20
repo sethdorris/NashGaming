1a144d5c49015b5a6edfcec2e2b89deb02571c22
sethdorris <sethc.dorris@gmail.com>
2015-12-19 19:03:54 -0600

    Created a Team Model

--- NashGaming/Models/Team.cs
+++ NashGaming/Models/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,12 @@ namespace NashGaming.Models
 {
     public class Team
     {
+        [Key]
         public int TeamID { get; set; }
         public string TeamName { get; set; }
         public List<Gamer> Roster { get; set; }
         public DateTime DateFounded { get; set; }
-        public Gamer Founder { get; set; }
+        public virtual Gamer Founder { get; set; }
         public int Rank { get; set; }
         public string Website { get; set; }
 
