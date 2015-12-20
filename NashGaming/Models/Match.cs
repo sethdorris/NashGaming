f4ba3a74a718d25fec9b49ceca40e3e3d4a816f5
sethdorris <sethc.dorris@gmail.com>
2015-12-19 19:03:27 -0600

    Created a Match model

--- NashGaming/Models/Match.cs
+++ NashGaming/Models/Match.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,16 @@ namespace NashGaming.Models
 {
     public class Match
     {
+        [Key]
         public int MatchID { get; set; }
-        
+        [Required]
+        public virtual Team Team1 { get; set; }
+        [Required]
+        public virtual Team Team2 { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Result { get; set; }
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
     }
 }
