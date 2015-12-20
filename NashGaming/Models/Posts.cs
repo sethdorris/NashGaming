9fcffaf1e0c26ecfcec4bf5a7a0c053fb75cecd1
sethdorris <sethc.dorris@gmail.com>
2015-12-19 19:03:39 -0600

    Created a Posts Model

---
+++ NashGaming/Models/Posts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NashGaming.Models
+{
+    public class Posts
+    {
+        [Key]
+        public int PostID { get; set; }
+        public virtual Gamer Author  { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
