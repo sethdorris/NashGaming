3b46f482c0e435df8328321c7f383b2b976f1ef4
sethdorris <sethc.dorris@gmail.com>
2015-12-19 19:03:10 -0600

    Created a Gamer Model

--- NashGaming/Models/Gamer.cs
+++ NashGaming/Models/Gamer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,12 @@ namespace NashGaming.Models
 {
     public class Gamer
     {
+        [Key]
         public int GamerID { get; set; }
+        public virtual ApplicationUser RealUser { get; set; }
         public string Handle { get; set; }
         public string Platform { get; set; }
-        public Team MemberOf { get; set; }
-        public string Game { get; set; }
+        public virtual Team MemberOf { get; set; }
+        
     }
 }
