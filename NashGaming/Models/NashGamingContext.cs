8227bdf7e56b2a5171fd01811c35749b42ad14b7
sethdorris <sethc.dorris@gmail.com>
2015-12-19 19:07:47 -0600

    Created NashGaming Context

---
+++ NashGaming/Models/NashGamingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NashGaming.Models
+{
+    public class NashGamingContext : ApplicationDbContext
+    {
+        public virtual DbSet<Gamer> Gamers { get; set; }
+        public virtual DbSet<Posts> Posts { get; set; }
+        public virtual DbSet<Match> Matches { get; set; }
+        public virtual DbSet<Team> Teams { get; set; }
+    }
+}
