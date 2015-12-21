5bf73fd70b681de7c481dad6fed28d49660d8b76
sethdorris <sethc.dorris@gmail.com>
2015-12-20 19:29:24 -0600

    Created tests for creating new instance of my repository

---
+++ NashGaming.Tests/Models/RepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NashGaming.Models;
+
+namespace NashGaming.Tests.Models
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void RepositoryTestsEnsureICanCreateAnInstance()
+        {
+            NashGamingRepository Repo = new NashGamingRepository();
+            Assert.IsNotNull(Repo);
+        }
+
+        [TestMethod]
+        public void RepositoryTestsEnsureICanCreateAnInstancePassingAContext()
+        {
+            NashGamingContext myContext = new NashGamingContext();
+            NashGamingRepository Repo = new NashGamingRepository(myContext);
+            Assert.AreEqual(myContext, Repo.Context);
+        }
+    }
+}
