b97c4460a36a307bd40828c6a30319a74b7d10ca
sethdorris <sethc.dorris@gmail.com>
2015-12-20 19:29:48 -0600

    Initial creation of the repository with two constructors.

---
+++ NashGaming/Models/NashGamingRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NashGaming.Models
+{
+    public class NashGamingRepository
+    {
+        private NashGamingContext context;
+        public NashGamingContext Context { get { return context; } }
+
+        public NashGamingRepository()
+        {
+            context = new NashGamingContext();
+        }
+
+        public NashGamingRepository(NashGamingContext a_context)
+        {
+            context = a_context;
+        }
+    }
+}
