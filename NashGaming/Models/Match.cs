2585bed4d6571ace3e03da18ed086bcf4553e68a
sethdorris <sethc.dorris@gmail.com>
2015-12-24 16:53:33 -0600

    Updated Match Model requiring Teams

--- NashGaming/Models/Match.cs
+++ NashGaming/Models/Match.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -12,7 +13,9 @@ namespace NashGaming.Models
         public int MatchID { get; set; }
         public DateTime Date { get; set; }
         public DateTime Time { get; set; }
+        [Required]
         public virtual Team Team1 { get; set; }
+        [Required]
         public virtual Team Team2 { get; set; }
         public string Result { get; set; }
         public int Team1Score { get; set; }
