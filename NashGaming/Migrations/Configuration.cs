7e37f888acd8bb1532e095e310ee217b5b0a493e
sethdorris <sethc.dorris@gmail.com>
2016-01-10 17:12:59 -0600

    Deleted Database and recreated intial database with Seed Method for leagues.

--- NashGaming/Migrations/Configuration.cs
+++ NashGaming/Migrations/Configuration.cs
@@ -1,18 +1,17 @@
 namespace NashGaming.Migrations
 {
+    using Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using NashGaming.Models;
-    using System.Collections.Generic;
 
     internal sealed class Configuration : DbMigrationsConfiguration<NashGaming.Models.NashGamingContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(NashGaming.Models.NashGamingContext context)
@@ -29,24 +28,13 @@ namespace NashGaming.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            context.Leagues.AddRange(
-                new List<League> { new League { LeagueName = "Siege", Platform = "PS4"},
-                                   new League { LeagueName = "COD", Platform = "PS4"},
-                                   new League { LeagueName = "COD", Platform = "XB1" }
-            });
-           
-
-            context.Teams.AddRange(
-                new List<Team> { new Team { Rank = 1, Active = true, TeamName = "Siege"},
-                                 new Team { Rank = 2, Active = true, TeamName = "Seed2"}
-            });
-
-            context.Gamers.AddRange(
-                new List<Gamer> { new Gamer { Handle = "StiffNasty" },
-                                  new Gamer { Handle = "Pizzle" }
-            });
-            
-
+            List<League> seedLeague = new List<League>
+            {
+                 new League { LeagueName = "COD" },
+                 new League { LeagueName = "Halo" },
+                 new League { LeagueName = "BF4" }
+            };
+            context.Leagues.AddRange(seedLeague);
         }
     }
 }
