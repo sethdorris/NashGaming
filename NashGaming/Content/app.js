0818315da881fa299bd50ebe83194614c8819e78
sethdorris <sethc.dorris@gmail.com>
2016-01-14 18:42:15 -0600

    Implementing Angular Module and Controller.

--- NashGaming/Content/app.js
+++ NashGaming/Content/app.js
@@ -1,1 +1,8 @@
-﻿
+﻿var main = angular.module("app", [], function () { });
+
+main.controller("leaguecontroller", function ($scope, $http) {
+    $http.get("/League/JsonLeagues")
+        .then(function (data) {
+            $scope.leaguenames = data.data;
+        });
+});
