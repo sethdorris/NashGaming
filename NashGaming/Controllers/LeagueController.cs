3cb0f38db9ee67df1a9b669703fe413945b2d166
sethdorris <sethc.dorris@gmail.com>
2016-01-14 18:42:44 -0600

    Created a new HttpGet Method that returns JSON of all the Leagues.

--- NashGaming/Controllers/LeagueController.cs
+++ NashGaming/Controllers/LeagueController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using NashGaming.Models;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace NashGaming.Controllers
 {
@@ -22,6 +24,13 @@ namespace NashGaming.Controllers
         {
             List<League> l = _repo.GetAllLeagues();
             return View(l);
+        }
+
+        [HttpGet]
+        public ActionResult JsonLeagues()
+        {
+            List<League> l = _repo.GetAllLeagues();
+            return Json(l, JsonRequestBehavior.AllowGet);
         }
     }
 }
