b8a9ee1f366a5542ef1d74d7cfb7b5bfbfe8053d
sethdorris <sethc.dorris@gmail.com>
2016-01-15 21:46:12 -0600

    Created a service for getting all the leagues from my database for angular

--- NashGaming/Content/app.js
+++ NashGaming/Content/app.js
@@ -1,8 +1,18 @@
 ﻿var main = angular.module("app", [], function () { });
 
-main.controller("leaguecontroller", function ($scope, $http) {
-    $http.get("/League/JsonLeagues")
-        .then(function (data) {
-            $scope.leaguenames = data.data;
-        });
+main.controller("leaguecontroller", function ($scope, leagueservice) {
+    console.log(leagueservice.getLeagues());
+}]);
+
+main.factory("leagueservice", function ($http) {
+    var allLeagues;
+    return {
+        getLeagues: function () {
+            $http.get("/League/JsonLeagues")
+                .then(function (object) {
+                    allLeagues = object.data;
+                });
+            return allLeagues;
+        }
+    }
 });
