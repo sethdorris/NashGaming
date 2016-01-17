0b962c5feac3ae174a63788995f0d7f3a419f6e8
sethdorris <sethc.dorris@gmail.com>
2016-01-16 20:33:39 -0600

    Reseeded database with gamer.

---
+++ NashGaming/Migrations/201601170204520_SeedGamer.cs
@@ -0,0 +1,16 @@
+namespace NashGaming.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedGamer : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
