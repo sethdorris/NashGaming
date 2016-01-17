487602bec2bdeef22b18f52aead8be0f70cb0e0b
sethdorris <sethc.dorris@gmail.com>
2016-01-16 19:23:29 -0600

    Added a constructor to set a repo and context, and added the public properties of a repo and context.

--- NashGaming/Controllers/HomeController.cs
+++ NashGaming/Controllers/HomeController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NashGaming.Models;
 
 namespace NashGaming.Controllers
 {
     public class HomeController : Controller
     {
+        public NashGamingContext _context { get; set; }
+        public NashGamingRepository _repo { get; set; }
+
+        public HomeController()
+        {
+            _context = new NashGamingContext();
+            _repo = new NashGamingRepository();
+        }
+        
         public ActionResult Index()
         {
             return View();
@@ -15,9 +25,9 @@ namespace NashGaming.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            List<Gamer> gamers = _repo.GetAllGamers();
 
-            return View();
+            return View(gamers);
         }
 
         public ActionResult Contact()
