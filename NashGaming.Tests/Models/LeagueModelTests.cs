c0a8c537521600c438ccd3417c3557e44a645790
sethdorris <sethc.dorris@gmail.com>
2016-05-24 10:47:42 -0600

    Added test class for League model

---
+++ NashGaming.Tests/Models/LeagueModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NashGaming.Models;
+
+namespace NashGaming.Tests.Models
+{
+    [TestClass]
+    public class LeagueModelTests
+    {
+        [TestMethod]
+        public void LeagueModelTestsEnsureICanCreateInstanceOfLeague()
+        {
+            League a = new League();
+            Assert.IsNotNull(a);
+        }
+
+        [TestMethod]
+        public void LeagueModelTestsEnsureAllPropsAreWorking()
+        {
+            League a = new League { Platform = "PS4", GameTitle = "RS6", LeagueID = 1, LeagueName = "2v2 TDM" };
+            Assert.AreEqual("PS4", a.Platform);
+            Assert.AreEqual("RS6", a.GameTitle);
+            Assert.AreEqual(1, a.LeagueID);
+            Assert.AreEqual("2v2 TDM", a.LeagueName);
+        }
+    }
+}
