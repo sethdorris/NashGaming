64f181f9a9f32535afba5c17f1925782fecb4199
sethdorris <sethc.dorris@gmail.com>
2016-05-25 13:49:13 -0600

    Added unit test for Team Invite class creation

---
+++ NashGaming.Tests/Models/TeamInviteTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NashGaming.Models;
+
+namespace NashGaming.Tests.Models
+{
+    [TestClass]
+    public class TeamInviteTests
+    {
+        [TestMethod]
+        public void TeamInviteTestsEnsureICanCreateAnInstance()
+        {
+            TeamInvite invite = new TeamInvite
+            {
+                Accepted = false,
+                TeamInviteID = 0,
+                DateSent = new DateTime().Date,
+                InvitedGamer = new Gamer { Handle = "Seth" },
+                Team = new Team { TeamName = "Sudsy" }
+            };
+
+            Assert.IsNotNull(invite);
+            Assert.AreEqual("Sudsy", invite.Team.TeamName);
+        }
+    }
+}
