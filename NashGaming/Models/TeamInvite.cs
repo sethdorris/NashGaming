0565eb59643a6b69d51957d39b9b7aa6b97a8ee1
sethdorris <sethc.dorris@gmail.com>
2016-05-25 13:44:18 -0600

    Created a TeamInvite class

---
+++ NashGaming/Models/TeamInvite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGaming.Models
+{
+    public class TeamInvite
+    {
+        [Key]
+        public int TeamInviteID { get; set; }
+        public virtual Team Team { get; set; }
+        public virtual Gamer InvitedGamer { get; set; }
+        public DateTime DateSent { get; set; }
+        public bool Accepted { get; set; }
+    }
+}
