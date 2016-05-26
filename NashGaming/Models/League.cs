40646e2055e35581ed70fd332d8638b1373435ae
sethdorris <sethc.dorris@gmail.com>
2016-05-26 15:54:32 -0600

    Implementing Equals method on League Class

--- NashGaming/Models/League.cs
+++ NashGaming/Models/League.cs
@@ -14,11 +14,25 @@ namespace NashGaming.Models
         public int MinPlayers { get; set; }
         public int MaxPlayers { get; set; }
         public string LeagueName { get; set; }
+        public int GamesPerWeek { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int SeasonLength { get; set; }
+        public string LeagueType { get; set; }
         public string GameTitle { get; set; }
         public virtual List<Match> Matches { get; set; }
         [MaxLength(3)]
         [MinLength(2)]
         public string Platform { get; set; }
         public virtual List<Posts> Feed { get; set; }
+        public override bool Equals(object obj)
+        {
+            League o = obj as League;
+            if (o == null)
+            {
+                return false;
+            }
+            return o.LeagueID == this.LeagueID;
+        }
     }
 }
