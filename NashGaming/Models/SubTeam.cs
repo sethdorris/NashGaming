04bcc89e2b59a533c167c465ecd1836fe3ed3fcd
sethdorris <sethc.dorris@gmail.com>
2016-05-26 11:31:07 -0600

    Created a SubTeam Model

---
+++ NashGaming/Models/SubTeam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGaming.Models
+{
+    public class SubTeam : IComparable
+    {
+        public int SubTeamID { get; set; }
+        public string SubTeamName { get; set; }
+        public MainTeam MainTeam { get; set; }
+        public virtual Gamer Captain { get; set; }
+        public virtual League League { get; set; }
+        public int Rank { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public virtual List<Gamer> Roster { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            SubTeam subteam = obj as SubTeam;
+            return this.SubTeamName.CompareTo(subteam.SubTeamName);
+        }
+    }
+}
