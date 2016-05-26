e088613db8798d464c3c11def31c696b3a66ecde
sethdorris <sethc.dorris@gmail.com>
2016-05-26 13:10:06 -0600

    Deleted handle prop and added Username prop in the IdentityModel

--- NashGaming/Models/IdentityModels.cs
+++ NashGaming/Models/IdentityModels.cs
@@ -9,7 +9,7 @@ namespace NashGaming.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string Handle { get; set; }
+        public string Username { get; set; }
         
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
