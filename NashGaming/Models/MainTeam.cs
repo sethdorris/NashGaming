83fdbb59d4c13c32b5011046f91098f816e7df40
sethdorris <sethc.dorris@gmail.com>
2016-05-26 11:30:44 -0600

    Created a MainTeam class

---
+++ NashGaming/Models/MainTeam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NashGaming.Models
+{
+    public class MainTeam : IComparable
+    {
+        [Key]
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public DateTime DateFounded { get; set; }
+        public virtual Gamer Founder { get; set; }
+        public string Website { get; set; }
+        public bool Active { get; set; }
+        public List<SubTeam> SubTeams { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            MainTeam team = obj as MainTeam;
+            return this.TeamName.CompareTo(team.TeamName);
+        }
+    }
+}
