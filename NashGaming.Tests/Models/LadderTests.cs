0e7060428c17501cc3245e6e6809a019b9f9b58a
sethdorris <sethc.dorris@gmail.com>
2016-05-27 09:59:09 -0600

    Created unit test for ladder model

---
+++ NashGaming.Tests/Models/LadderTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NashGaming.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGaming.Tests.Models
+{
+    [TestClass]
+    public class LadderTests
+    {
+        [TestMethod]
+        public void LadderTestsEnsureICanCreateAnInstance()
+        {
+            Ladder l = new Ladder { LadderID = 0, GameTitle = "Halo", MaxPlayers = 2 };
+            Assert.IsNotNull(l);
+            Assert.AreEqual(0, l.LadderID);
+            Assert.AreEqual("Halo", l.GameTitle);
+        }
+    }
+}
