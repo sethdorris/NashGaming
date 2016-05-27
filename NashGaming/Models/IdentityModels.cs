b2b3ecf111b753dcf20be1a1a3049043a186f7c7
unknown <Spencer.Nichols@ABQL009.ruralsource.com>
2016-05-27 12:28:17 -0600

    Added appropriate properties to obtain the users xbox gamer tag and/or PSNID

--- NashGaming/Models/IdentityModels.cs
+++ NashGaming/Models/IdentityModels.cs
@@ -10,8 +10,10 @@ namespace NashGaming.Models
     public class ApplicationUser : IdentityUser
     {
         public string Username { get; set; }
-        
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+		public string XbxGamertag { get; set; }
+		public string  PSNID { get; set; }
+
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
