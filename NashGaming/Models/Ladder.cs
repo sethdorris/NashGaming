b044656209d077c322497686c7e0b2ab8dcbab8c
sethdorris <sethc.dorris@gmail.com>
2016-05-27 09:55:48 -0600

    Created a Ladder model

---
+++ NashGaming/Models/Ladder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashGaming.Models
+{
+    public class Ladder
+    {
+        public int LadderID { get; set; }
+        public string LadderName { get; set; }
+        public string GameTitle { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+        public string Platform { get; set; }
+        public virtual List<Posts> Feed { get; set; }
+        public virtual List<SubTeam> Teams { get; set; }
+        public virtual List<Match> Matches { get; set; }
+        public override bool Equals(object obj)
+        {
+            Ladder a = obj as Ladder;
+            if (obj == null)
+            {
+                return false;
+            }
+            return a.LadderID == this.LadderID;
+        }
+    }
+}
