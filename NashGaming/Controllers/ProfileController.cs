5b7521156b9ff07b9c70a05d4ad842a117d72241
sethdorris <sethc.dorris@gmail.com>
2016-06-02 16:14:30 -0600

    Added a Profile Controller and an Index View for Profile

---
+++ NashGaming/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using NashGaming.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NashGaming.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private NashGamingRepository _repo;
+        private NashGamingContext _context;
+        public ProfileController()
+        {
+            _repo = new NashGamingRepository();
+            _context = new NashGamingContext();
+        }
+        // GET: Profile
+        public ActionResult Index()
+        {
+            Gamer g = _repo.GetGamerByAspUserName(User.Identity.Name);
+            return View(g);
+        }
+    }
+}
